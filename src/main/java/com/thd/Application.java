f1c75119e86cc387fce9c1b32da0a25ef17365ae
devil13th <wl_18190>
2018-07-06 00:30:37 +0800

    ..

--- src/main/java/com/thd/Application.java
+++ src/main/java/com/thd/Application.java
@@ -1,10 +1,15 @@
 package com.thd;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -54,4 +59,16 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 	    SpringApplication.run(Application.class, args);
 	}
+	
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
+
+	@Bean
+	public SessionFactory getSessionFactory() {
+	    if (entityManagerFactory.unwrap(SessionFactory.class) == null) {
+	        throw new NullPointerException("factory is not a hibernate factory");
+	    }
+	    return entityManagerFactory.unwrap(SessionFactory.class);
+	}
+	
 }
--- src/main/java/com/thd/note/dao/EntityDaoImpl.java
+++ src/main/java/com/thd/note/dao/EntityDaoImpl.java
@@ -1,8 +1,12 @@
 package com.thd.note.dao;
 
+import javax.annotation.Resource;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 @Repository(value="entityDaoImpl")  
 public class EntityDaoImpl implements EntityDao {
@@ -10,8 +14,13 @@ public class EntityDaoImpl implements EntityDao {
 	@PersistenceContext
     private EntityManager entityManager;
 	
+	@Resource
+	private DataSource dataSource;
+	@Autowired
+	private SessionFactory sessionFactory;
 	@Override
 	public void add(Object obj) {
+		System.out.println(sessionFactory);
 		entityManager.persist(obj);
 	}
 
